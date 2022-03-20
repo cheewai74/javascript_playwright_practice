a5d8ca5989ca408dc89f78498ba5f3c796657234
moses <academicsnotes@gmail.com>
2022-03-20 23:49:51 +0800

    Commit script

---
+++ play/dropdown.js
@@ -0,0 +1,27 @@
+const {chromium} = require('playwright');
+
+( async() => {
+    //function code
+    const browser = await chromium.launch({headless:false, slowMo: 200});
+    const page = await browser.newPage();
+    await page.goto('https://the-internet.herokuapp.com/dropdown');
+
+    const dropdown = await page.$('#dropdown');
+    // value 
+    // await dropdown.selectOption({value: '1'});
+
+    // // label
+    // await dropdown.selectOption({label: 'Option2'});
+
+    // index
+    await dropdown.selectOption({index: 2});
+
+    // values inside this select
+    // $$ : Retrieve more than 1 element
+    const availableOptions = await dropdown.$$('option');
+    for (let i = 0; i < availableOptions.length; i++){
+        console.log(await availableOptions[i].innerText())
+    }
+
+    await browser.close();
+})();
