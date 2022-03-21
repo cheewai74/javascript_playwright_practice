742b33fd6fa0f41256b61df8fc7dce1437a96e56
moses <academicsnotes@gmail.com>
2022-03-21 12:19:24 +0800

    Commit scripts

---
+++ play/checkradio.js
@@ -0,0 +1,19 @@
+const {chromium} = require('playwright');
+
+( async() => {
+    //function code
+    const browser = await chromium.launch({headless:false, slowMo: 300});
+    const page = await browser.newPage();
+    await page.goto('https://www.w3schools.com/howto/howto_css_custom_checkbox.asp');
+
+    // Check the 2nd checkbox
+    const checks = await page.$$('#main div:nth-child(1) input[type="checkbox"]');
+    await checks[1].check();
+    await checks[0].uncheck();
+
+    // Select the 2nd radio button
+    // const radios = await page.$$('#main div:nth-child(1) input[type="radio"]');
+    // await radios[1].check();
+  
+    await browser.close();
+})();
